240ee2470023482203950066521f97c0d348b91c
JuanCRM <jcros84@gmail.com>
2020-05-17 19:38:32 +0200

    Primera subida a Producción

---
+++ src/Controller/InicioController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InicioController extends AbstractController
+{
+    /**
+     * @Route("/inicio", name="inicio")
+     */
+    public function index()
+    {
+        return $this->render('default/default.html.twig');
+    }
+
+    /**
+     * @Route("/minor", name="minor")
+     */
+    public function minor()
+    {
+        return $this->render('default/minor.html.twig');
+
+    }
+
+}
